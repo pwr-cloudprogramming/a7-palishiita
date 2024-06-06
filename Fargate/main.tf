f1accb4e5cbc233dea96d8fb43c9444c0140aac4
EC2 Default User <ec2-user@ip-172-31-21-201.ec2.internal>
2024-06-06 06:22:29 +0000

    important changes in fargate

--- Fargate/main.tf
+++ Fargate/main.tf
@@ -1,6 +1,6 @@
 # terraform services
 terraform {
- required_providers {
+required_providers {
         aws = {
             source  = "hashicorp/aws"
             version = ">= 5.0"
@@ -171,191 +171,191 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-#Load balancer
-resource "aws_alb" "main" {
-  name        = "tictactoe-alb"
-  subnets         = [aws_subnet.tictactoe_subnet1.id, aws_subnet.tictactoe_subnet2.id]
-  security_groups = [aws_security_group.alb_sg.id]
-}
+# #Load balancer
+# resource "aws_alb" "main" {
+#   name        = "tictactoe-alb"
+#   subnets         = [aws_subnet.tictactoe_subnet1.id, aws_subnet.tictactoe_subnet2.id]
+#   security_groups = [aws_security_group.alb_sg.id]
+# }
 
-resource "aws_alb_target_group" "alb-target-gr-back" {
-  name        = "alb-target-gr-back"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.tictactoe_vpc.id
-  target_type = "ip"
+# resource "aws_alb_target_group" "alb-target-gr-back" {
+#   name        = "alb-target-gr-back"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = aws_vpc.tictactoe_vpc.id
+#   target_type = "ip"
 
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "200"
-    timeout             = "3"
-    path                = "/"
-    unhealthy_threshold = "2"
-  }
-}
+#   health_check {
+#     healthy_threshold   = "3"
+#     interval            = "30"
+#     protocol            = "HTTP"
+#     matcher             = "200"
+#     timeout             = "3"
+#     path                = "/health"
+#     unhealthy_threshold = "2"
+#   }
+# }
 
-resource "aws_alb_target_group" "alb-target-gr-front" {
-  name        = "falb-target-gr-front"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.tictactoe_vpc.id
-  target_type = "ip"
+# resource "aws_alb_target_group" "alb-target-gr-front" {
+#   name        = "falb-target-gr-front"
+#   port        = 80
+#   protocol    = "HTTP"
+#   vpc_id      = aws_vpc.tictactoe_vpc.id
+#   target_type = "ip"
 
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "200"
-    timeout             = "3"
-    path                = "/"
-    unhealthy_threshold = "2"
-  }
-}
+#   health_check {
+#     healthy_threshold   = "3"
+#     interval            = "30"
+#     protocol            = "HTTP"
+#     matcher             = "200"
+#     timeout             = "3"
+#     path                = "/"
+#     unhealthy_threshold = "2"
+#   }
+# }
 
-# Redirect all traffic from the ALB to the target group
-resource "aws_alb_listener" "alb_listener_backend" {
-  load_balancer_arn = aws_alb.main.id
-  port              = 8080
-  protocol          = "HTTP"
+# # Redirect all traffic from the ALB to the target group
+# resource "aws_alb_listener" "alb_listener_backend" {
+#   load_balancer_arn = aws_alb.main.id
+#   port              = 8080
+#   protocol          = "HTTP"
 
-  default_action {
-    target_group_arn = aws_alb_target_group.alb-target-gr-back.id
-    type             = "forward"
-  }
-}
+#   default_action {
+#     target_group_arn = aws_alb_target_group.alb-target-gr-back.id
+#     type             = "forward"
+#   }
+# }
 
-resource "aws_alb_listener" "alb_listener_frontend" {
-  load_balancer_arn = aws_alb.main.id
-  port              = 80
-  protocol          = "HTTP"
+# resource "aws_alb_listener" "alb_listener_frontend" {
+#   load_balancer_arn = aws_alb.main.id
+#   port              = 80
+#   protocol          = "HTTP"
 
-  default_action {
-    target_group_arn = aws_alb_target_group.alb-target-gr-front.id
-    type             = "forward"
-  }
-}
+#   default_action {
+#     target_group_arn = aws_alb_target_group.alb-target-gr-front.id
+#     type             = "forward"
+#   }
+# }
 
-resource "aws_ecs_cluster" "tictactoe_cluster" {
-  name = "TictactoeCluster"
-}
+# resource "aws_ecs_cluster" "tictactoe_cluster" {
+#   name = "TictactoeCluster"
+# }
 
-resource "aws_ecs_task_definition" "tictactoe_backend_task" {
-  family                   = "TictactoeBackend"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
-  execution_role_arn       = "arn:aws:iam::851725643716:role/LabRole"
-  task_role_arn            = "arn:aws:iam::851725643716:role/LabRole"
+# resource "aws_ecs_task_definition" "tictactoe_backend_task" {
+#   family                   = "TictactoeBackend"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   cpu                      = "512"
+#   memory                   = "1024"
+#   execution_role_arn       = "arn:aws:iam::975050228305:role/LabRole" 
+#   task_role_arn            = "arn:aws:iam::975050228305:role/LabRole"
 
-  container_definitions = jsonencode([
-    {
-      name        = "backend"
-      image       = "851725643716.dkr.ecr.us-east-1.amazonaws.com/tictactoe_back:fargate"
-      essential   = true
-      portMappings = [
-        {
-          containerPort = 8080
-          hostPort      = 8080
-          protocol      = "tcp"
-        }
-      ]
-      environment = [
-        {
-          name  = "ALB_DNS_NAME"
-          value = "${aws_alb.main.dns_name}"
-        }
-      ]
-    }
-  ])
-}
+#   container_definitions = jsonencode([
+#     {
+#       name        = "backend"
+#       image       = "975050228305.dkr.ecr.us-east-1.amazonaws.com/tictactoe-backend:fargate"
+#       essential   = true
+#       portMappings = [
+#         {
+#           containerPort = 8080
+#           hostPort      = 8080
+#           protocol      = "tcp"
+#         }
+#       ]
+#       environment = [
+#         {
+#           name  = "ALB_DNS_NAME"
+#           value = "${aws_alb.main.dns_name}"
+#         }
+#       ]
+#     }
+#   ])
+# }
 
-# Frontend Task Definition
-resource "aws_ecs_task_definition" "tictactoe_frontend_task" {
-  family                   = "TictactoeFrontend"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "1024"
-  execution_role_arn       = "arn:aws:iam::851725643716:role/LabRole"
-  task_role_arn            = "arn:aws:iam::851725643716:role/LabRole"
+# # Frontend Task Definition
+# resource "aws_ecs_task_definition" "tictactoe_frontend_task" {
+#   family                   = "TictactoeFrontend"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   cpu                      = "512"
+#   memory                   = "1024"
+#   execution_role_arn       = "arn:aws:iam::975050228305:role/LabRole"
+#   task_role_arn            = "arn:aws:iam::975050228305:role/LabRole"
 
-  container_definitions = jsonencode([
-    {
-      name        = "frontend"
-      image       = "851725643716.dkr.ecr.us-east-1.amazonaws.com/tictactoe_front:fargate"
-      essential   = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-          protocol      = "tcp"
-        }
-      ]
-      environment = [
-        {
-          name  = "BACKEND_ADDRESS"
-          value = "${aws_alb.main.dns_name}"
-        }
-      ]
-    }
-  ])
-}
+#   container_definitions = jsonencode([
+#     {
+#       name        = "frontend"
+#       image       = "975050228305.dkr.ecr.us-east-1.amazonaws.com/tictactoe-frontend"
+#       essential   = true
+#       portMappings = [
+#         {
+#           containerPort = 80
+#           hostPort      = 80
+#           protocol      = "tcp"
+#         }
+#       ]
+#       environment = [
+#         {
+#           name  = "BACKEND_ADDRESS"
+#           value = "${aws_alb.main.dns_name}"
+#         }
+#       ]
+#     }
+#   ])
+# }
 
 
-# Backend Service
-resource "aws_ecs_service" "tictactoe_backend_service" {
-  name            = "TictactoeBackendService"
-  cluster         = aws_ecs_cluster.tictactoe_cluster.id
-  task_definition = aws_ecs_task_definition.tictactoe_backend_task.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
+# # Backend Service
+# resource "aws_ecs_service" "tictactoe_backend_service" {
+#   name            = "TictactoeBackendService"
+#   cluster         = aws_ecs_cluster.tictactoe_cluster.id
+#   task_definition = aws_ecs_task_definition.tictactoe_backend_task.arn
+#   launch_type     = "FARGATE"
+#   desired_count   = 1
   
-  network_configuration {
-    subnets          = [aws_subnet.tictactoe_subnet1.id, aws_subnet.tictactoe_subnet2.id]
-    security_groups  = [aws_security_group.backend_sg.id]
-    assign_public_ip = true
-  }
+#   network_configuration {
+#     subnets          = [aws_subnet.tictactoe_subnet1.id, aws_subnet.tictactoe_subnet2.id]
+#     security_groups  = [aws_security_group.backend_sg.id]
+#     assign_public_ip = true
+#   }
   
-  load_balancer {
-    target_group_arn = aws_alb_target_group.alb-target-gr-back.id
-    container_name   = "backend"
-    container_port   = 8080
-  }
+#   load_balancer {
+#     target_group_arn = aws_alb_target_group.alb-target-gr-back.id
+#     container_name   = "backend"
+#     container_port   = 8080
+#   }
 
-  depends_on = [aws_ecs_task_definition.tictactoe_backend_task, aws_alb_listener.alb_listener_backend]
-}
+#   depends_on = [aws_ecs_task_definition.tictactoe_backend_task, aws_alb_listener.alb_listener_backend]
+# }
 
-resource "aws_ecs_service" "tictactoe_frontend_service" {
-  name            = "TictactoeFrontendService"
-  cluster         = aws_ecs_cluster.tictactoe_cluster.id
-  task_definition = aws_ecs_task_definition.tictactoe_frontend_task.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
+# resource "aws_ecs_service" "tictactoe_frontend_service" {
+#   name            = "TictactoeFrontendService"
+#   cluster         = aws_ecs_cluster.tictactoe_cluster.id
+#   task_definition = aws_ecs_task_definition.tictactoe_frontend_task.arn
+#   launch_type     = "FARGATE"
+#   desired_count   = 1
   
-  network_configuration {
-    subnets          = [aws_subnet.tictactoe_subnet1.id, aws_subnet.tictactoe_subnet2.id]
-    security_groups  = [aws_security_group.frontend_sg.id]
-    assign_public_ip = true
-  }
+#   network_configuration {
+#     subnets          = [aws_subnet.tictactoe_subnet1.id, aws_subnet.tictactoe_subnet2.id]
+#     security_groups  = [aws_security_group.frontend_sg.id]
+#     assign_public_ip = true
+#   }
   
-  load_balancer {
-    target_group_arn = aws_alb_target_group.alb-target-gr-front.id
-    container_name   = "frontend"
-    container_port   = 80
-  }
+#   load_balancer {
+#     target_group_arn = aws_alb_target_group.alb-target-gr-front.id
+#     container_name   = "frontend"
+#     container_port   = 80
+#   }
 
-  depends_on = [aws_ecs_task_definition.tictactoe_frontend_task, aws_alb_listener.alb_listener_frontend]
-}
+#   depends_on = [aws_ecs_task_definition.tictactoe_frontend_task, aws_alb_listener.alb_listener_frontend]
+# }
 
 
-#output
-output "alb_backend" {
-  value = "http://${aws_alb.main.dns_name}:8080"
-}
+# #output
+# output "alb_backend" {
+#   value = "http://${aws_alb.main.dns_name}:8080"
+# }
 
-output "alb_frontend" {
-  value = "http://${aws_alb.main.dns_name}:80"
-}
+# output "alb_frontend" {
+#   value = "http://${aws_alb.main.dns_name}:80"
+# }
